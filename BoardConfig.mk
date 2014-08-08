ba606ed04fb3a2c1c4869570fcacc89b916ccab6
nuclearmistake <nuclearmistake@gmail.com>
2014-08-08 10:31:28 -0400

    Fixup cm reorg, and temporarily switch to cm-11.0's kernel
    Change-Id: Ia569e5fc506254d2e759150165a4a38c812bcf52

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,10 +46,8 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.f
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := jf_eur_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_jf_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
-TARGET_KERNEL_VARIANT_CONFIG := KT_jf_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := jfselinux_defconfig
 
 # Audio
 BOARD_HAVE_AUDIENCE_ES325_2MIC := true
